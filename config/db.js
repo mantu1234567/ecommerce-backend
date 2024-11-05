fd8e5aaf0a552fd9ff636566f46d56fdba0f46b6
Mantu kumar <mantukumar803204@gmail.com>
2024-11-05 20:39:22 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
-// /config/db.js
-
 const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env file
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://mantukumar803204:DJWRMIQa5OLJQuYE@ecommerce.oa06o.mongodb.net/', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
